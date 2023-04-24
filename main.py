1a9fc00672e295000b907869bf55b83ed6ee7540
MLTime2Code <mia.larushka.05@gmail.com>
2023-04-23 19:55:39 -0500

    Updated and improved home and menu functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import mysql.connector
 
 connection = mysql.connector.connect(user = 'root', database = 'example', password = 'SQL1Sh4rd2USE*')
@@ -5,7 +6,7 @@ connection = mysql.connector.connect(user = 'root', database = 'example', passwo
 cursor = connection.cursor()
 
 
-testQuery = ("SELECT * FROM pokemon")
+testQuery = ("SELECT * FROM user_table")
 
  
 
@@ -22,3 +23,46 @@ for item in cursor:
 cursor.close()
 
 connection.close()
+
+def home():
+  print('Welcome!')
+  print()
+  print('1--Log In')
+  print('2--Create an account')
+  print('3--Exit')
+  print()
+  print(choice)
+  choice = input('What would you like to do?')
+
+  if choice == 1:
+     print('Please input your username and PIN')
+     print('Usernam: ')
+     print('PIN: ')
+  elif choice == 2:
+     print('A usernam and PIN will be randomly generated for you.')
+  elif choice == 3:
+     exit()
+  else:
+     print('1,2, & 3 are your only options. Try again.')
+     return home
+
+
+
+
+def menu():
+    print('1--Check Balance')
+    print('2--Deposit')
+    print('3--Withdraw')
+    print()
+    print(decision)
+    decision = input('What would you like to do?')
+    
+    
+
+#def balance():
+
+#def deposit():
+
+
+#def withdraw():
+
